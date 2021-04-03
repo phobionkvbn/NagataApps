4d9f93a3ec773affe8ebae4d01c0bfce3d1f0eb5
phobionkvbn <11950115160@students.uin-suska.ac.id>
2021-04-03 22:27:07 +0700

    tugasday3

---
+++ app/src/main/java/com/example/nagataapps/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.nagataapps;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tampilan_menu);
+    }
+
+    public void clickRS(View view) {
+        //klik gambar rumah sakit
+        Intent i = new Intent(this,RS.class);
+        startActivity(i);
+    }
+}
